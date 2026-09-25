bf40e08496040dc9f21e7c07b3a0134be52e6ea9
Juan21JR <juanjesus54.rojas@gmail.com>
2026-09-24 19:40:35 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,84 @@
+const CACHE_NAME = "cafeteria-cecytem-v2";
+
+const ARCHIVOS = [
+    "./",
+    "./index.html",
+    "./cafeteria.css",
+    "./manifest.json",
+
+    "./imagenes/cecylogo.jpg",
+    "./imagenes/icon-192.png",
+    "./imagenes/icon-512.png",
+
+    "./imagenes/imagestacosdorados.jpg",
+    "./imagenes/tortajamon.jpg",
+    "./imagenes/imagesyogurt.jpg",
+    "./imagenes/Hamburguesa.jpg",
+    "./imagenes/imagesburrito.jpg",
+    "./imagenes/imageshotcakes.jpg",
+    "./imagenes/imagesquesadillas.jpg",
+    "./imagenes/imageshotdog.jpg",
+    "./imagenes/imageslicuado.jpg",
+    "./imagenes/imagestortamilanesa.jpg",
+    "./imagenes/imagestacosbistec.jpg",
+    "./imagenes/imagesensaladafrutas.jpg",
+    "./imagenes/imagespizza.jpg",
+    "./imagenes/clubsandwich.jpg",
+    "./imagenes/waffles.jpg"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => {
+
+                return cache.addAll(ARCHIVOS);
+
+            })
+
+    );
+
+    self.skipWaiting();
+
+});
+
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(nombres => {
+
+            return Promise.all(
+
+                nombres
+                    .filter(nombre => nombre !== CACHE_NAME)
+                    .map(nombre => caches.delete(nombre))
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(respuesta => {
+
+                return respuesta || fetch(event.request);
+
+            })
+
+    );
+
+});
